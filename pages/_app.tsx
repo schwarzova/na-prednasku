3b5b43fc736b68ace78f5fd50402829397c8d62f
Schwarzová, Tereza <schwarzova@cngroup.dk>
2022-05-26 15:34:08 +0200

    Remove next-goatcounter

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,8 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
-import { GCScript } from 'next-goatcounter';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <GCScript
-        siteUrl={'https://naprednasku.goatcounter.com/count'}
-        scriptSrc=""
-      />
-      <Component {...pageProps} />
-    </>
-  );
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
