76bae75cd41f512a4fd0c29392cc037db68d417f
Schwarzová, Tereza <schwarzova@cngroup.dk>
2022-05-26 16:44:39 +0200

    Views per week

--- pages/api/goat-counter.ts
+++ pages/api/goat-counter.ts
@@ -1,7 +1,7 @@
 export const getPageViews = async () => {
   try {
     const views = await fetch(
-      'https://naprednasku.goatcounter.com/counter//.json'
+      'https://naprednasku.goatcounter.com/counter//.json?start=week'
     );
     const { count_unique } = await views.json();
     return count_unique;
