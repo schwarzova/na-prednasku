24ac947ec78d3acdad17aee43b3c9b262b466540
Schwarzová, Tereza <schwarzova@cngroup.dk>
2022-05-31 19:50:00 +0200

    Add GA

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from 'next/document';
+import Script from 'next/script';
 
 export default function Document() {
   return (
@@ -9,6 +10,20 @@ export default function Document() {
           async
           src="//gc.zgo.at/count.js"
         ></script>
+        <Script
+          strategy="lazyOnload"
+          src="https://www.googletagmanager.com/gtag/js?id=G-Z70KEM3G63"
+        ></Script>
+        <Script id="google-analytics" strategy="lazyOnload">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-Z70KEM3G63', {
+              page_path: window.location.pathname,
+            });
+                `}
+        </Script>
       </Head>
       <body>
         <Main />
