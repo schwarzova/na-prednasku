dbdc50919214de0aead3fd0171a6232c7c304c65
Schwarzová, Tereza <schwarzova@cngroup.dk>
2022-11-08 10:55:02 +0100

    Add socials, tw, ig, newsletter

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,14 +4,32 @@ function Footer() {
   return (
     <footer className="px-3 py-5 bg-neutral-900 text-slate-400 sm:px-10 flex flex-col justify-between sm:flex-row">
       info@nabifli.cz
-      <a
-        className="text-transparent bg-clip-text bg-gradient-to-r from-indigo-600 to-red-400 hover:from-indigo-600 hover:to-indigo-600"
-        href="https://www.instagram.com/naprednasku/"
-        target="_blank"
-        rel="noreferrer"
-      >
-        instagram
-      </a>
+      <div className="flex">
+        <a
+          className="text-transparent bg-clip-text bg-gradient-to-r from-blue-600 to-blue-800 hover:from-blue-600 hover:to-blue-600"
+          href="https://twitter.com/na_prednasku"
+          target="_blank"
+          rel="noreferrer"
+        >
+          twitter
+        </a>
+        <a
+          className="text-transparent bg-clip-text bg-gradient-to-r from-indigo-600 to-red-400 hover:from-indigo-600 hover:to-indigo-600 mx-5"
+          href="https://www.instagram.com/naprednasku/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          instagram
+        </a>
+        <a
+          className="text-transparent bg-clip-text bg-gradient-to-r from-green-600 to-yellow-400 hover:from-yellow-400 hover:to-yellow-400"
+          href="http://eepurl.com/ic63M5"
+          target="_blank"
+          rel="noreferrer"
+        >
+          newsletter
+        </a>
+      </div>
       <span className="text-sm mt-5 sm:mt-0">
         made by{' '}
         <a
