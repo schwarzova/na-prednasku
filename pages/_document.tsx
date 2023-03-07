bb0cc0b793bf80c7354957a59fd0d4d383407242
Schwarzová, Tereza <schwarzova@cngroup.dk>
2023-03-07 18:25:13 +0100

    Set ga tag

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -22,6 +22,15 @@ export default function Document() {
           `,
           }}
         />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-N79V2Q7');
+        `}
+        </Script>
         <script
           data-goatcounter="https://naprednasku.goatcounter.com/count"
           async
@@ -32,6 +41,14 @@ export default function Document() {
         <Main />
         <NextScript />
       </body>
+      <noscript>
+        <iframe
+          src="https://www.googletagmanager.com/ns.html?id=GTM-N79V2Q7"
+          height="0"
+          width="0"
+          style={{ display: 'none', visibility: 'hidden' }}
+        ></iframe>
+      </noscript>
     </Html>
   );
 }
