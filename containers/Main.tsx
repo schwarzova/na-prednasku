33b88d1a4e95bd9677fe6b6d0f1c66ddb1f76f88
Schwarzová, Tereza <schwarzova@cngroup.dk>
2023-08-16 22:37:03 +0200

    Fix banner for mobile

--- containers/Main.tsx
+++ containers/Main.tsx
@@ -90,14 +90,15 @@ function Main(props: Props) {
         onSearchChange={handleSearch}
         searchText={searchText}
       />
-      <div className="flex justify-center mb-8 hover:opacity-90">
+      <div className="flex justify-center sm:mb-8 mb-4 hover:opacity-90">
         <a href="https://www.nabifli.cz/" target="_blank" rel="noreferrer">
-          <Image
+          <img
+            className="md:h-[120px] md:w-[1000px] object-cover h-[80px] w-[400px]"
             src="/nabifliBanner.png"
             alt="bart"
-            width="1000"
-            height="140"
-            style={{ borderRadius: '12px' }}
+            style={{
+              borderRadius: '12px',
+            }}
           />
         </a>
       </div>
